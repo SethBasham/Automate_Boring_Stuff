5e8546e68faf33a2ccc2916e99f51e8ce619fda6
SethBasham <Basham.e.Seth@gmail.com>
2020-06-01 21:42:54 -0600

    Initial Commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# This program says 'Hello' and asks for users name.
+
+print('Hello World!')
+name = input('What is your name? ') # Asks the user for their name.
+print(f'It is good to meet you, {name}.')
+print(f'The length of your name is {len(name)}.')
+age = input('What is your age? ') # Asks the user for their age.
+print(f'You will be {int(age) + 1} in a year.')
---
+++ vampire.py
@@ -0,0 +1,11 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice!')
+elif age < 12:
+    print('YOu are not Alice, Kiddo!')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, imortal vampire.')
+elif age > 100:
+    print('You are not Alice, Grannie!')
+    
