4d55926399f888e1cb83b8ec1c07a0b7e18a8d96
Seth Basham <seth@debox.home>
2020-06-21 22:17:41 -0600

    yourName added

---
+++ littleKid.py
@@ -0,0 +1,9 @@
+name = 'Carol'
+age = 30000
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print('You are not Alice, Kiddo!')
+else:
+    print('You are neither Alice or a little kid.')
+    
