c9a1b5907900da502a7ca75a469cda11b26a2fa0
Seth Basham <basham.e.seth@gmail.com>
2020-07-23 21:51:23 -0600

    learning lists

---
+++ allMyCats2.py
@@ -0,0 +1,13 @@
+catNames = []
+
+while True:
+    print('Enter the name of your cat ' + str(len(catNames) + 1) + 
+        ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames += [name] # List concatenation
+
+print('The cat names are: ')
+for name in catNames:
+    print('\t' + name)
